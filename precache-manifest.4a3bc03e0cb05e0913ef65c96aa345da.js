2725ac8207870d3f2edcc76babd9d161d33a347f
Lee kwangsung <igwangseong@LEEui-MacBook-Pro.local>
2018-10-17 04:22:30 +0900

    Updates

---
+++ precache-manifest.4a3bc03e0cb05e0913ef65c96aa345da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "d09f8593a20c55497ba7",
+    "url": "/movie-app/static/js/main.d09f8593.chunk.js"
+  },
+  {
+    "revision": "d931867c60fd0f725508",
+    "url": "/movie-app/static/js/1.d931867c.chunk.js"
+  },
+  {
+    "revision": "d09f8593a20c55497ba7",
+    "url": "/movie-app/static/css/main.cdd9bee5.chunk.css"
+  },
+  {
+    "revision": "ae6da883873038debb17654d1e5d470d",
+    "url": "/movie-app/index.html"
+  }
+];
